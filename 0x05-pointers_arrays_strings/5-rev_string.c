7d837d15aff26f2f2b2621ebbf0474b81392c40e
Adeoye Raji <82328855+Frapisky@users.noreply.github.com>
2022-06-14 20:35:32 +0100

    new code 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int n = 0, i = 0;
+	char tmp;
+
+	while (s[i++])
+		n++;
+
+	for (i = (n - 1); i >= (n / 2); i--)
+	{
+		tmp = s[i];
+		s[i] = s[n - i - 1];
+		s[n - i - 1] = tmp;
+	}
+}
