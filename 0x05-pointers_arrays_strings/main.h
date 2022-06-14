c7727461fd82dbf47417df5fbde97af53885c1c8
Adeoye Raji <82328855+Frapisky@users.noreply.github.com>
2022-06-14 20:20:19 +0100

    my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 /**
 * Desc: Header file containing prototypes for all functions
 *       used in the 0x04-pointers_arrays_strings directory.
