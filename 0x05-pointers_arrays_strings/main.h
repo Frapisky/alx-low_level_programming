9ae4696be7709511a4db618bbbee419e461d8daa
Adeoye Raji <82328855+Frapisky@users.noreply.github.com>
2022-06-14 20:29:01 +0100

    my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,10 @@
-#ifndef main_h
-#define main_h
-/**
-* Desc: Header file containing prototypes for all functions
-*       used in the 0x04-pointers_arrays_strings directory.
-*/
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -17,4 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 #endif
