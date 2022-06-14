5ffb2537fb2ff0daad03593669f7d46c532fcfc8
Adeoye Raji <82328855+Frapisky@users.noreply.github.com>
2022-06-14 20:30:53 +0100

    my commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,15 @@
 #include "main.h"
+
 /**
-* reset_to_98 - Takes a pointer to an int and updates
-*               the value it points to to 98.
-* @n: The pointer to an int.
-*/
-void reset_to_98(int *n)
+ *swap_int - swaps the values of two numbers
+ *@a: first number
+ *@b: second number
+ */
+
+void swap_int(int *a, int *b)
 {
-*n = 98;
+	int c = *a;
+
+	*a = *b;
+	*b = c;
 }
